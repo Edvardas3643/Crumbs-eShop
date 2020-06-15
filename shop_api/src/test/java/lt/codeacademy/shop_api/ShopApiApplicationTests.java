9cab786db82eeb31484693adeea02a10289b3852
Edvardas3643 <Edvardas3643@gmail.com>
2020-06-15 14:03:40 +0300

    Added: shop_api initial commit

---
+++ shop_api/src/test/java/lt/codeacademy/shop_api/ShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.shop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
